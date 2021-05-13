b7af6b5b50eb85241500716b662077e1a391dd56
Lucianf5 <vedita_luciano@live.com>
2021-05-13 19:55:46 -0300

    Atualização do funcionario

--- src/projeto/funcionario.java
+++ src/projeto/funcionario.java
@@ -1,12 +1,56 @@
-package projeto;
+package Teste;
 
-public class funcionario {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class funcionario extends pessoa{
+	
+	private int cod;
+	private String cpf;
+	private String endereco;
+	
+	public funcionario(String nome, String email, int telefone, int idade, int cod, String cpf,String endereco)
+	{
+		super(nome, email, telefone, idade);
+		this.cod = cod;
+		this.cpf =cpf;
+		this.endereco = endereco;
+	}
+	
+	public String getInfo()
+	{
+		return "\nNome do funcionário: " + super.getNome() + "\nEmail: " + super.getEmail() + "\nIdade: " 
+		+ super.getIdade() + "\nTelefone: " + super.getTelefone() + "\nCódigo do funcionário: " + this.getCod() ;
 		
+	}
+	public String getInfoCliente()
+	{
+		return "\nNome do cliente: " + super.getNome() + "\nEmail: " + super.getEmail() + "\nIdade: " 
+		+ super.getIdade() + "\nTelefone: " + super.getTelefone() + "\nCódigo: " + this.getCod() +  "\nCPF: " + this.getCpf()
+		+"\nEndereço: " + this.getEndereco();
 		
-		System.out.println("Hello World!");
+	}
+	
+	public int getCod() {
+		return cod;
 	}
 
+
+	public void setCod(int cod) {
+		this.cod = cod;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
 }
+
