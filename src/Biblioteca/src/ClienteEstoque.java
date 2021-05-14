4a16404b992d3295a6d15548976aacf7993f1958
ThamiresHonorato <thamireshonorato1998@gmail.com>
2021-05-13 21:30:26 -0300

    att thamires

--- src/Biblioteca/src/ClienteEstoque.java
+++ src/Biblioteca/src/ClienteEstoque.java
@@ -1,57 +1,94 @@
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.*;
 
-class ClienteEstoque {
+	
+	class ClienteEstoque {
 
-	public static void main(String[] args) {
-		List<String> categoria = new ArrayList<String>();
-		int op;
-		categoria.add("A menina que roubava livro ");
-		categoria.add("Do mil ao milhão");
-		categoria.add("Cronicas de Narnia");
-		categoria.add("Espelho Espelho meu");
-		Scanner leia = new Scanner(System.in);
+		public static void main(String[] args) {
+			List<String> drama = new ArrayList<String>();
+			int op;
+			
+			drama.add("O sol é para todos ");
+			drama.add("Sociedade do cansaço");
+			drama.add("Admiravel mundo novo");
+			drama.add("Em busca de nos mesmo");
+			
+			List<String> terror = new ArrayList<String>();
+			
+			terror.add("Dracula ");
+			terror.add("It - A coisa");
+			terror.add("O Exorcista");
+			terror.add("O cemitério");
+			
 
-		do {
-			System.out.println("\n-------------------------------");
-			System.out.println("\n(1) Deseja ver livro de comedia ?");
-			System.out.println("\n(2) Deseja remover este livro ?");
-			System.out.println("\n(3) Atualizar lista de livro ?");
-			System.out.println("\n(4) Mostrar todos os livros ?");
-			System.out.println("\n(0) Deseja encerrar o programa?");
-			System.out.println("\nDigite sua opção: ");
-			op = leia.nextInt();
-			switch (op) {
+			List<String> comedia = new ArrayList<String>();
+		
+			comedia.add("A Troca ");
+			comedia.add("Como ser mulher");
+			comedia.add("A divina comedia");
+			comedia.add("Minha vida não tão perfeita");
+			
+			List<String> romance = new ArrayList<String>();
+			
+			romance.add("O Duque e Eu ");
+			romance.add("Anne of Green Gables");
+			romance.add("A cinco passos de você");
+			romance.add("Sol da meia-noite");
+			
+			Scanner leia = new Scanner(System.in);
+			do {
+				System.out.println("\n-------------------------------");
+				System.out.println("\n(1) Deseja ver livros de terror ?");
+				System.out.println("\n(2) Deseja ver livros de drama ?");
+				System.out.println("\n(3) Deseja ver livros de comedia ?");
+				System.out.println("\n(4) Deseja ver livros de romance ?");
+				System.out.println("\n(5) Ver todas as categorias!");
+				System.out.println("\n(0) Deseja encerrar o programa?");
+				System.out.println("\nDigite sua opção: ");
+				op = leia.nextInt();
+				
+				switch (op) {
 
-			case 1:
+				case 1:
 
-				System.out.println("\nMostrando os livros categoria comedia ...");
-				System.out.println(categoria);
-				break;
+					System.out.println("\nMostrando os livros categoria drama ...");
+					System.out.println(drama);
+					break;
 
-			case 2:
+				case 2:
 
-				System.out.println("\nMostrando os livros categoria drama ...");
-				System.out.println(categoria);
-				break;
+					System.out.println("\nMostrando os livros categoria terror ...");
+					System.out.println(terror);
+					break;
 
-			case 3:
+				case 3:
 
-				System.out.println("\nDeseja atualizar...");
-				System.out.println(categoria);
-				break;
+					System.out.println("\nMostrando livros de comedia...");
+					System.out.println(comedia);
+					break;
 
-			case 4:
+				case 4:
 
-				System.out.println("\nDeseja ver todos os livros...");
-				System.out.println(categoria);
-				break;
-			}
+					System.out.println("\nMostrando livros de romance ...");
+					System.out.println(romance);
+					break;
+					
+				case 5:
 
-		} while (op != 0);
-		System.out.println("\nFinalizou o programa...");
+					System.out.println("CATEGORIAS:");
+					System.out.println("\nDRAMA > "+drama);
+					System.out.println("\nTERROR > "+terror);
+					System.out.println("\nCOMÉDIA > "+comedia);
+					System.out.println("\nROMANCE> "+romance);
+				}
+
+			} while (op != 0);
+			System.out.println("\nFinalizou o programa...");
+
+		}
 
 	}
 
-}
+
